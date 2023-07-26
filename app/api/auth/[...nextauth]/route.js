db78560773e23443982a8b103bd7a6e5e1e19b32
adekunle-A <31294512+adekunle-A@users.noreply.github.com>
2023-07-26 11:47:57 -0400

    updated folder name

---
+++ app/api/auth/[...nextauth]/route.js
@@ -0,0 +1,46 @@
+import NextAuth from "next-auth";
+import User from "@models/users";
+import { connectToDB } from "@utils/db";
+import GoogleProvider from "next-auth/providers/google";
+
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SCERET,
+    }),
+  ],
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({
+        email: session.user.email,
+      });
+      session.user.id = sessionUser._id.toString();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        //serverless -> lambda -> dynamodb(only call when needed)
+        await connectToDB();
+        //check if a user already exists
+        const userExist = await User.findOne({
+          email: profile.email,
+        });
+        //if not, create a new user and save to db
+        if (!userExist) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+        return true;
+      } catch (error) {
+        console.log(error);
+        return false;
+      }
+    },
+  },
+});
+
+export { handler as GET, handler as POST };
